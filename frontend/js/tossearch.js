781c807bf3ad114bf0dc4625b641353ba849cd6c
hiiwave <hiiwave@gmail.com>
2018-03-06 11:11:58 +0800

    Added chinese datasource

--- frontend/js/tossearch.js
+++ frontend/js/tossearch.js
@@ -1,4 +1,4 @@
-var items = new Bloodhound({
+var items_en = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.whitespace,
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   // url points to a json file that contains an array of country names, see
@@ -6,13 +6,25 @@ var items = new Bloodhound({
   prefetch: 'data/names_en.json'
 });
 
+var items_tw = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: 'data/names_tw.json'
+});
 
 $(document).ready(function () {
   console.log("ready!");
   // passing in `null` for the `options` arguments will result in the default
   // options being used
   $('#prefetch .typeahead').typeahead(null, {
-    name: 'items',
-    source: items
+    name: 'items-en',
+    source: items_en
+  }, {
+    name: 'items-tw',
+    source: items_tw
+  });
+
+  $('.typeahead').bind('typeahead:select', function (ev, suggestion) {
+    console.log('Selection: ' + suggestion);
   });
 });
